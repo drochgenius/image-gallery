fd95faf93de2640a6cc1bbdc2a15074fde7b4c93
Dominique Rochefort <drochefort@tribalnova.com>
2018-07-18 16:48:38 -0400

    use css custom properties

--- src/components/image-gallery.ts
+++ src/components/image-gallery.ts
@@ -2,15 +2,13 @@ import { html, LitElement } from '@polymer/lit-element/lit-element';
 import { TemplateResult } from 'lit-html/lit-html';
 
 export class ImageGallery extends LitElement {
-    public slide: number = 0;
-    public stageHeight: number;
-    public stageWidth: number;
+    public position: number = 0;
     public statement: string = 'Hello World';
     private slideCount: number = 0;
 
     static get properties(): { [key: string]: string | object } {
         return {
-            slide: Number,
+            position: Number,
             slideCount: Number,
             stageHeight: Number,
             stageWidth: Number,
@@ -18,10 +16,34 @@ export class ImageGallery extends LitElement {
         };
     }
 
+    public styles(position: number, slideCount: number): TemplateResult {
+        const stageWidth = getComputedStyle(this as any).getPropertyValue('--stage-width');
+
+        return html`
+            .flex {
+                display: flex;
+                align-items: center; 
+                margin-left: ${-(position % slideCount) * parseInt(stageWidth)}px;
+                transition: margin-left 1s ease-in-out;
+            }
+
+            .stage {
+                width: var(--stage-width, 100%);
+                height: var(--stage-height,100vh);
+                overflow: hidden;
+            }
+
+            ::slotted(figure){
+                margin: 0;
+                padding: auto;
+                height: var(--stage-height,100vh);
+                min-width: var(--stage-width, 100%);
+            }`;
+    }
+
     public _firstRendered() {
         console.log('calling first rendered');
         const slot: HTMLSlotElement = this.shadowRoot.querySelector('slot');
-
         const nodes: Node[] = slot.assignedNodes();
 
         for (const el of nodes as HTMLElement[]) {
@@ -29,42 +51,29 @@ export class ImageGallery extends LitElement {
             el.querySelector('img').style.height = 'inherit';
         }
 
-        this.slideCount = nodes.length;
-        setInterval(() => this.slide++, 2000);
+        setInterval(() => this.position++, 2000);
     }
 
-    protected _render({ statement, stageWidth, stageHeight, slide, slideCount }: ImageGallery): TemplateResult {
-        const css: TemplateResult = html`
-        .flex {
-            display: flex;
-            align-items: center; 
-            margin-left: ${-(slide % slideCount) * stageWidth}px;
-            transition: margin-left 1s ease-in-out;
-        }
-
-        .stage {
-            width: ${stageWidth}px;
-            height: ${stageHeight}px;
-            overflow: hidden;
-        }
-
-        ::slotted(figure){
-            margin: 0;
-            padding: 0;
-            height: ${stageHeight}px;
-            min-width: ${stageWidth}px;
-        }`;
-
-        const style: TemplateResult = html`<style>${css}</style>`;
+    protected _render({ statement, position, slideCount }: ImageGallery): TemplateResult {
+        const style: TemplateResult = html`<style>${this.styles(position, slideCount)}</style>`;
 
         return html`
             ${style}
             <h3>${statement}</h3>
             <div class="stage">
                 <div class="flex">
-                    <slot name="items"></slot>
+                    <slot name="items" on-slotchange="${(e: Event) => this.slotChanged(e)}"></slot>
                 </div>
             </div>`;
+    }
+
+    private slotChanged(event: Event) {
+        const slot: HTMLSlotElement = event.srcElement as HTMLSlotElement;
+        if (slot) {
+            this.slideCount = slot.assignedNodes().length;
+        }
+
+        console.log('SLOT CHANGED', this.slideCount);
     }
 }
 
