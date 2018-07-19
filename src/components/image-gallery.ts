8709a1bd5a1df042f4c7e103d6595093ea5e28ac
Dominique Rochefort <drochefort@tribalnova.com>
2018-07-19 10:08:08 -0400

    cleanup and add support for text and video and other content types

--- src/components/image-gallery.ts
+++ src/components/image-gallery.ts
@@ -1,65 +1,43 @@
 import { html, LitElement } from '@polymer/lit-element/lit-element';
 import { TemplateResult } from 'lit-html/lit-html';
 
+const DEFAULT_STAGE_WIDTH: string = '640px';
+
 export class ImageGallery extends LitElement {
+    public activityTitle: string = 'Hello World';
     public position: number = 0;
-    public statement: string = 'Hello World';
-    private slideCount: number = 0;
+    private itemCount: number = 0;
 
     static get properties(): { [key: string]: string | object } {
         return {
             position: Number,
-            slideCount: Number,
-            stageHeight: Number,
-            stageWidth: Number,
-            statement: String
+            itemCount: Number,
+            activityTitle: String
         };
     }
 
-    public styles(position: number, slideCount: number): TemplateResult {
-        const stageWidth = getComputedStyle(this as any).getPropertyValue('--stage-width');
+    public get stageWidth(): string {
+        return getComputedStyle(this as any).getPropertyValue('--stage-width') || DEFAULT_STAGE_WIDTH;
+    }
 
+    public get styles(): TemplateResult {
         return html`
             .flex {
-                display: flex;
-                align-items: center; 
-                margin-left: ${-(position % slideCount) * parseInt(stageWidth)}px;
-                transition: margin-left 1s ease-in-out;
-            }
-
-            .stage {
-                width: var(--stage-width, 100%);
-                height: var(--stage-height,100vh);
-                overflow: hidden;
-            }
-
-            ::slotted(figure){
-                margin: 0;
-                padding: auto;
-                height: var(--stage-height,100vh);
-                min-width: var(--stage-width, 100%);
+                margin-left: ${-(this.position % this.itemCount) * parseInt(this.stageWidth)}px;
             }`;
     }
 
     public _firstRendered() {
-        console.log('calling first rendered');
-        const slot: HTMLSlotElement = this.shadowRoot.querySelector('slot');
-        const nodes: Node[] = slot.assignedNodes();
-
-        for (const el of nodes as HTMLElement[]) {
-            el.removeAttribute('hidden');
-            el.querySelector('img').style.height = 'inherit';
-        }
-
         setInterval(() => this.position++, 2000);
     }
 
-    protected _render({ statement, position, slideCount }: ImageGallery): TemplateResult {
-        const style: TemplateResult = html`<style>${this.styles(position, slideCount)}</style>`;
+    protected _render({ activityTitle }: ImageGallery): TemplateResult {
+        const style: TemplateResult = html`<style>${this.styles}</style>`;
 
         return html`
+            <link rel="stylesheet" type="text/css" href="/dist/css/image-gallery.css">
             ${style}
-            <h3>${statement}</h3>
+            <h3>${activityTitle}</h3>
             <div class="stage">
                 <div class="flex">
                     <slot name="items" on-slotchange="${(e: Event) => this.slotChanged(e)}"></slot>
@@ -67,13 +45,24 @@ export class ImageGallery extends LitElement {
             </div>`;
     }
 
+    /**
+     * Update the UI whenever nodes are added or removed from the slot
+     *
+     * @param event
+     */
     private slotChanged(event: Event) {
         const slot: HTMLSlotElement = event.srcElement as HTMLSlotElement;
+        const nodes: Node[] = slot.assignedNodes();
         if (slot) {
-            this.slideCount = slot.assignedNodes().length;
+            this.itemCount = nodes.length;
+            for (const el of nodes as HTMLElement[]) {
+                el.removeAttribute('hidden');
+                const img: HTMLImageElement = el.querySelector('img');
+                if (img) {
+                    el.querySelector('img').style.height = 'inherit';
+                }
+            }
         }
-
-        console.log('SLOT CHANGED', this.slideCount);
     }
 }
 
