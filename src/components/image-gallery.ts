3d0b392be237d3b25ab14a0380031e7adc58a916
Dominique Rochefort <drochefort@tribalnova.com>
2018-07-19 14:23:54 -0400

    add methods to navigate the slide show

--- src/components/image-gallery.ts
+++ src/components/image-gallery.ts
@@ -1,15 +1,19 @@
 import { html, LitElement } from '@polymer/lit-element/lit-element';
 import { TemplateResult } from 'lit-html/lit-html';
 
+const DEFAULT_DELAY: number = 2000;
 const DEFAULT_STAGE_WIDTH: string = '640px';
 
 export class ImageGallery extends LitElement {
+    public autoPlay: boolean = false;
     public activityTitle: string = 'Hello World';
     public position: number = 0;
     private itemCount: number = 0;
+    private interval: NodeJS.Timer;
 
     static get properties(): { [key: string]: string | object } {
         return {
+            autoPlay: Boolean,
             position: Number,
             itemCount: Number,
             activityTitle: String
@@ -21,14 +25,54 @@ export class ImageGallery extends LitElement {
     }
 
     public get styles(): TemplateResult {
-        return html`.flex { margin-left: ${-(this.position % this.itemCount) * parseInt(this.stageWidth)}px; }`;
+        return html`.flex { margin-left: ${-this.position * parseInt(this.stageWidth)}px; }`;
     }
 
-    public _firstRendered() {
-        setInterval(() => this.position++, 2000);
+    /**
+     * Play the slideshow, switching automatically after the specified delay
+     *
+     * @param delay : time to wait before switching to the next slide
+     * @param reverse : reverse playback direction
+     */
+    public play(delay: number = DEFAULT_DELAY, reverse: boolean = false): void {
+        this.pause();
+        this.interval = setInterval(() => (reverse ? this.prev() : this.next()), delay);
+    }
+
+    public pause(): void {
+        clearInterval(this.interval);
+    }
+
+    public next(): ImageGallery {
+        this.position = (this.position + 1) % this.itemCount;
+        return this;
+    }
+
+    public prev(): ImageGallery {
+        this.position = (this.itemCount + (this.position - 1)) % this.itemCount;
+        return this;
+    }
+
+    public goto(position: number = 0): void {
+        if (Math.abs(position) > this.itemCount) {
+            position = 0;
+        }
+
+        if (position >= 0) {
+            this.position = position % this.itemCount;
+        } else {
+            this.position = (this.itemCount + position) % this.itemCount;
+        }
+    }
+
+    public _firstRendered(): void {
+        if (this.autoPlay) {
+            this.play();
+        }
     }
 
     protected _render({ activityTitle }: ImageGallery): TemplateResult {
+        console.log('POSITION IS', this.position);
         const style: TemplateResult = html`<style>${this.styles}</style>`;
 
         return html`
@@ -47,7 +91,7 @@ export class ImageGallery extends LitElement {
      *
      * @param event
      */
-    private slotChanged(event: Event) {
+    private slotChanged(event: Event): void {
         const slot: HTMLSlotElement = event.srcElement as HTMLSlotElement;
         const nodes: Node[] = slot.assignedNodes();
         if (slot) {
